ffcc83374d8efa30847ce33b6a0a77f48a3971e8
BillZong <billzong@163.com>
2019-11-13 14:20:28 +0800

    add git remove object from repo history script
    Should use it with git-lean.sh to minimize the .git directory

---
+++ shell/git-rm-object.sh
@@ -0,0 +1,6 @@
+if [ $# -lt 1 ];then
+  echo "Usage: $0 object-to-remove-from-current-repo"
+  exit -1
+fi
+
+git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
