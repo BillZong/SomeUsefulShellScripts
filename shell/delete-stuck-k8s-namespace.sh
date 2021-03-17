28b8f81d74e72a19afa9e51b6ed20524de6cb186
BillZong <billzong@163.com>
2021-03-17 16:35:25 +0800

    add k8s namespace delete hang handle shell script

---
+++ shell/delete-stuck-k8s-namespace.sh
@@ -0,0 +1,4 @@
+NS=$1
+kubectl get namespace $NS -o json \
+            | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+            | kubectl replace --raw /api/v1/namespaces/$NS/finalize -f -
