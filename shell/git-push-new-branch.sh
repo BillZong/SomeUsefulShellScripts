3fec358030e7119df811fdf21c9dd7f0fe0bda23
BillZong <billzong@163.com>
2021-05-31 11:05:03 +0800

    feature/git: push new branch and set it as upstream

---
+++ shell/git-push-new-branch.sh
@@ -0,0 +1,4 @@
+REMOTE_REPO=${1:-origin}
+#git push --set-upstream $REMOTE_REPO `git rev-parse --abbrev-ref HEAD`
+# or git version >= 2.22
+git push --set-upstream $REMOTE_REPO `git branch --show-current`
