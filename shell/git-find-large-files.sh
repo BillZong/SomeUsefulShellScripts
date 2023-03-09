f427378228384d374230a1ec5732614dd2251a3b
BillZong <billzong@163.com>
2023-03-09 18:07:16 +0800

    Add directory option to git find large files

--- shell/git-find-large-files.sh
+++ shell/git-find-large-files.sh
@@ -1,7 +1,42 @@
-git rev-list --objects --all |
-  git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
+# Bash Version 3 required (it also works with ksh)
+[[ ${BASH_VERSINFO[0]} -lt 3 ]] && exit 1
+
+# Defaults
+dir="."
+# for skipping value
+skip=''
+
+# Put all arguments in a new array (because BASH_ARGV is read only)
+ARGS=( "$@" )
+
+for i in "${!ARGS[@]}"; do
+  [[ -n "$skip" ]] && {
+    skip=''
+    continue
+  }
+  case "${ARGS[i]}" in
+    -d|--dir|--directory)
+      # Use +1 to access next array element and unset it
+      dir="${ARGS[i+1]}"
+      unset 'ARGS[i]'
+      skip=1
+      ;;
+    --)
+      # End of arguments
+      unset 'ARGS[i]'
+      break
+      ;;
+    *)              
+      # Skip unset if our argument has not been matched
+      continue
+      ;;
+  esac
+  unset 'ARGS[i]'
+done
+
+git -C $dir rev-list --objects --all |
+  git -C $dir cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
   sed -n 's/^blob //p' |
   sort --numeric-sort --key=2 |
   cut -c 1-12,41- |
   $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
-
