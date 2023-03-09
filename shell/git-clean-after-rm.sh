129ce0a6bd068026b55d4c80f99a1e3e3abe2e1c
BillZong <billzong@163.com>
2023-03-09 18:15:37 +0800

    Script: clean local git cache after removing git object

---
+++ shell/git-clean-after-rm.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Bash Version 3 required (it also works with ksh)
+[[ ${BASH_VERSINFO[0]} -lt 3 ]] && exit 1
+
+# Defaults
+dir="."
+branch="master"
+# for skipping value
+skip=''
+
+# Put all arguments in a new array (because BASH_ARGV is read only)
+ARGS=( "$@" )
+
+for i in "${!ARGS[@]}"; do
+  [[ -n "$skip" ]] && {
+    skip=''
+    continue
+  }
+  case "${ARGS[i]}" in
+    -d|--dir|--directory)
+      # Use +1 to access next array element and unset it
+      dir="${ARGS[i+1]}"
+      unset 'ARGS[i]'
+      skip=1
+      ;;
+    -b|--branch)
+      branch="${ARGS[i+1]}"
+      unset 'ARGS[i]'
+      ;;
+    --)
+      # End of arguments
+      unset 'ARGS[i]'
+      break
+      ;;
+    *)              
+      # Skip unset if our argument has not been matched
+      continue
+      ;;
+  esac
+  unset 'ARGS[i]'
+done
+
+pushd $dir
+
+# update local ref
+git update-ref -d refs/original/refs/heads/$branch
+git reflog expire --expire=now --all
+git gc --prune=now
+
+popd
