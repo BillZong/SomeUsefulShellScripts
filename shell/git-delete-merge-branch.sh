6ac4b8e615ac157a3d140093f65a16bf70b1d4e8
Bill Zong <billzong2012@gmail.com>
2023-04-23 15:33:12 +0800

    bash: Test merged branch deletion (#5)
    * more safety delete of merged branches
    
    * more comments

--- shell/git-delete-merge-branch.sh
+++ shell/git-delete-merge-branch.sh
@@ -1,1 +1,14 @@
-git branch -v | grep "\[gone\]" | awk '{print $1}' | while read b; do git branch -D $b; done
+#!/usr/bin/env bash
+
+# Bash Version 3 required (it also works with ksh)
+[[ ${BASH_VERSINFO[0]} -lt 3 ]] && exit 1
+
+# Default git directory.
+git_dir=${1-"."}
+
+# The proper way to delete branches
+git -C $git_dir branch --merged | egrep -v "(^\*|master|main|develop|dev)" | xargs -L1 git -C $git_dir branch -d
+
+# The wrong way to delete branches
+## git -C $git_dir branch -v | grep "\[gone\]" | awk '{print $1}' | while read b; if [ -n "$b" ]; then do git -C $git_dir branch -D $b; fi; done
+## git -C $git_dir branch -v | grep "\[gone\]" | awk '{print $1}' | xargs -L1 git -C $git_dir branch -D
