6688448bb58c9fb9bbd3fc343d45781d3b125ac9
BillZong <billzong@163.com>
2024-04-26 14:50:41 +0800

    find and query git status of sub directories

---
+++ shell/git-status-subdir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 {DIRECTORY} {DEPTH}"
+	exit 1
+fi
+
+DIRECTORY=$1
+DEPTH=$2
+
+# perfect one
+find $DIRECTORY -type d -depth $DEPTH \
+	\! -name "\.*" \
+	-exec echo "Working directory: "{} \; \
+	-exec git --git-dir={}/.git --work-tree=$PWD/{} status \; \
+    -exec echo "" \;
